b4824cc7e0cb29e2d696db354b3fd6c59feb3093
KarishmaJhaveri019 <95085648+KarishmaJhaveri019@users.noreply.github.com>
2023-01-25 10:34:54 +0530

    Update String.java

--- String.java
+++ String.java
@@ -1,6 +1,6 @@
 public class String {
 	    public static void main(String[] args) {
-	        String str = "This is Karishma Jhaveri";
+	        String str = "Karishma";
 	        int length = str.length();
 	        System.out.println("Length of the string: " + length);
 	    }
